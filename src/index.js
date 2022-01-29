825c7de68a5000087b6d605e97c5d764fb005c14
DanilShapilov <danil@shapilov.dev>
2022-01-29 13:51:44 +0500

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false
+  }
+  const bracketsMap = Object.fromEntries(bracketsConfig)
+
+  const stack = []
+  for (const char of str) {
+    const openCloseSame = char === bracketsMap[char]
+    if (bracketsMap[char] && !openCloseSame) {
+      stack.push(char)
+    } else if (openCloseSame) {
+      const lastInStack = stack[stack.length - 1]
+      if (lastInStack === char) {
+        stack.pop()
+      } else {
+        stack.push(char)
+      }
+    } else {
+      const lastInStack = stack.pop()
+      if (bracketsMap[lastInStack] !== char) {
+        return false
+      }
+    }
+  }
+  return stack.length === 0
 }
